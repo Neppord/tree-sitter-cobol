72e18148e5583d2f1efdb9ea1620a67995042e4b
Samuel Ytterbrink <neppord@gmail.com>
2020-06-20 12:40:59 +0200

    Start using tree sitter primitives in identification division

--- grammar.js
+++ grammar.js
@@ -4,23 +4,25 @@ module.exports = grammar({
     rules: {
         source_file: $ => $.identification_division,
         // identification division is optional in MF
-        identification_division: $ => (
+        identification_division: $ => seq(
             "IDENTIFICATION DIVISION." // or "ID DIVISION." in MF OSVS and VSC2
-            + "PROGRAM-ID. program-name"
-            // 'is _ program' are optional
-            // "IS COMMON PROGRAM" ans85
-            // "IS INITIAL PROGRAM" ans85
-            // "IS EXTERNAL PROGRAM" MF
-            // optional
-            + "AUTHOR. comment-entry"
-            // optional
-            + "INSTALLATION. comment-entry"
-            // optional
-            + "DATE-WRITTEN. comment-entry"
-            // optional
-            + "DATE-COMPILED. comment-entry"
-            // optional
-            + "SECURITY. comment-entry"
+            , seq(
+                "PROGRAM-ID. program-name"
+                , optional(
+                    //  ans85
+                    choice(
+                        "IS COMMON PROGRAM"
+                        , "IS INITIAL PROGRAM"
+                    )
+                    // "IS EXTERNAL PROGRAM" MF
+                )
+            )
+
+            , optional("AUTHOR. comment-entry")
+            , optional("INSTALLATION. comment-entry")
+            , optional("DATE-WRITTEN. comment-entry")
+            , optional("DATE-COMPILED. comment-entry")
+            , optional("SECURITY. comment-entry")
             // only in OSVS where it is optional
             // + "REMARKS. comment-entry"
         )
