4b40b4c4fee0f5ea1b4bf4a198a2ed4f7b0effd0
Samuel Ytterbrink <neppord@gmail.com>
2020-06-20 12:28:17 +0200

    Add rough sketch of the identification division.

--- grammar.js
+++ grammar.js
@@ -1,8 +1,28 @@
 module.exports = grammar({
-    name: 'YOUR_LANGUAGE_NAME',
+    name: 'COBOL',
 
     rules: {
-        // TODO: add the actual grammar rules
-        source_file: $ => 'hello'
+        source_file: $ => $.identification_division,
+        // identification division is optional in MF
+        identification_division: $ => (
+            "IDENTIFICATION DIVISION." // or "ID DIVISION." in MF OSVS and VSC2
+            + "PROGRAM-ID. program-name"
+            // 'is _ program' are optional
+            // "IS COMMON PROGRAM" ans85
+            // "IS INITIAL PROGRAM" ans85
+            // "IS EXTERNAL PROGRAM" MF
+            // optional
+            + "AUTHOR. comment-entry"
+            // optional
+            + "INSTALLATION. comment-entry"
+            // optional
+            + "DATE-WRITTEN. comment-entry"
+            // optional
+            + "DATE-COMPILED. comment-entry"
+            // optional
+            + "SECURITY. comment-entry"
+            // only in OSVS where it is optional
+            // + "REMARKS. comment-entry"
+        )
     }
 });
