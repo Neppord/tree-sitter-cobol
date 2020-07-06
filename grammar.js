52944017f04362633baed93a62d068866641fdd0
Samuel Ytterbrink <neppord@gmail.com>
2020-07-06 15:28:59 +0200

    Increase compile speed by extracting subrules
    Seams especially efficient to group keywords with there optional small-words.

--- grammar.js
+++ grammar.js
@@ -25,21 +25,30 @@ module.exports = grammar({
     identification_division: ($) =>
       seq(
         "IDENTIFICATION DIVISION.",
-        seq(
-          "PROGRAM-ID.",
-          field("program_name", $.program_name),
-          op(or("IS COMMON PROGRAM", "IS INITIAL PROGRAM"))
-        ),
-
+        $._identification_division_program_id,
         op(
-          seq("AUTHOR.", field("author", $.comment_entry)),
-          seq("INSTALLATION.", field("installation", $.comment_entry)),
-          seq("DATE-WRITTEN.", field("date_written", $.comment_entry)),
-          seq("DATE-COMPILED.", field("date_compiled", $.comment_entry)),
-          seq("SECURITY.", field("security", $.comment_entry))
+          $._identification_division_author,
+          $._identification_division_installation,
+          $._identification_division_date_written,
+          $._identification_division_date_compiled,
+          $._identification_division_security
         )
-        // only in OSVS where it is optional
-        // + "REMARKS. comment-entry"
+      ),
+    _identification_division_author: ($) =>
+      seq("AUTHOR.", field("author", $.comment_entry)),
+    _identification_division_installation: ($) =>
+      seq("INSTALLATION.", field("installation", $.comment_entry)),
+    _identification_division_date_written: ($) =>
+      seq("DATE-WRITTEN.", field("date_written", $.comment_entry)),
+    _identification_division_date_compiled: ($) =>
+      seq("DATE-COMPILED.", field("date_compiled", $.comment_entry)),
+    _identification_division_security: ($) =>
+      seq("SECURITY.", field("security", $.comment_entry)),
+    _identification_division_program_id: ($) =>
+      seq(
+        "PROGRAM-ID.",
+        field("program_name", $.program_name),
+        op($._IS, or("COMMON", "INITIAL"), "PROGRAM")
       ),
     environment_division: ($) =>
       seq(
@@ -76,29 +85,28 @@ module.exports = grammar({
         $._sort_merge_file
       ),
     _record_sequential_file_assign: ($) =>
+      seq($._ASSIGN_TO, $._record_sequential_file_assign_destination),
+    _record_sequential_file_assign_destination: ($) =>
+      or(
+        seq(
+          op($._external_or_dynamic, $._record_sequential_file_type),
+          $._file_reference
+        ),
+        seq("DISC", "FROM", $.data_name)
+      ),
+    _record_sequential_file_type: ($) =>
+      or(
+        $._line_advancing,
+        "DISC",
+        "KEYBOARD",
+        "DISPLAY",
+        "PRINTER",
+        "PRINTER-1"
+      ),
+    _line_advancing: (_) =>
       seq(
-        "ASSIGN",
-        op("TO"),
-        or(
-          seq(
-            op(
-              $._external_or_dynamic,
-              or(
-                seq(
-                  or("LINE ADVANCING", seq(op("MULTIPLE"), or("REEL", "UNIT"))),
-                  op("FILE")
-                ),
-                "DISC",
-                "KEYBOARD",
-                "DISPLAY",
-                "PRINTER",
-                "PRINTER-1"
-              )
-            ),
-            $._file_reference
-          ),
-          seq("DISC", "FROM", $.data_name)
-        )
+        or("LINE ADVANCING", seq(op("MULTIPLE"), or("REEL", "UNIT"))),
+        op("FILE")
       ),
     _record_sequential_file_padding: ($) =>
       seq(
@@ -116,50 +124,46 @@ module.exports = grammar({
         $._select,
         $._record_sequential_file_assign,
         op(
-          seq("RESERVE", $.integer, $._area),
-          seq(op(seq("ORGANIZATION", optional($._IS))), "SEQUENTIAL"),
+          $._relative_file_reserve,
+          $._record_sequential_file_organization,
           $._record_sequential_file_padding,
-          seq(
-            "RECORD DELIMITER",
-            optional($._IS),
-            or("STANDARD-1", "character-string")
-          ),
-          seq("ACCESS", op("MODE"), optional($._IS), "SEQUENTIAL"),
+          $._record_sequential_file_record_delimiter,
+          $._record_sequential_file_record_access_mode,
           $._file_status
         )
       ),
+    _record_sequential_file_record_access_mode: ($) =>
+      seq($._ACCESS_MODE_IS, "SEQUENTIAL"),
+    _record_sequential_file_record_delimiter: ($) =>
+      seq(
+        "RECORD DELIMITER",
+        optional($._IS),
+        or("STANDARD-1", "character-string")
+      ),
+    _record_sequential_file_organization: ($) =>
+      seq(op(seq("ORGANIZATION", optional($._IS))), "SEQUENTIAL"),
+    _ACCESS_MODE_IS: ($) => seq("ACCESS", op("MODE"), optional($._IS)),
+    _RELATIVE_KEY_IS: ($) => seq("RELATIVE", optional("KEY"), optional($._IS)),
     _relative_file_access_mode: ($) =>
       seq(
-        "ACCESS",
-        op("MODE"),
-        optional($._IS),
+        $._ACCESS_MODE_IS,
         or(
-          seq(
-            "SEQUENTIAL",
-            op(seq("RELATIVE", op("KEY"), optional($._IS), $.data_name))
-          ),
-          seq(
-            or("RANDOM", "DYNAMIC"),
-            "RELATIVE",
-            op("KEY"),
-            optional($._IS),
-            $.data_name
-          )
+          seq("SEQUENTIAL", op(seq($._RELATIVE_KEY_IS, $.data_name))),
+          seq(or("RANDOM", "DYNAMIC"), $._RELATIVE_KEY_IS, $.data_name)
         )
       ),
     _relative_file_assign: ($) =>
       seq(
-        "ASSIGN",
-        op("TO"),
+        $._ASSIGN_TO,
         or(
           seq(op($._external_or_dynamic, "DISK"), $._file_reference),
           seq("DISK FROM", $.data_name),
           op($._external_or_dynamic)
         )
       ),
-    _relative_file_reserve: ($) => seq("RESERVE", $.integer, $._area),
+    _relative_file_reserve: ($) => seq("RESERVE", $.integer, $._AREA),
     _select: ($) => seq("SELECT", op("OPTIONAL"), $.file_name),
-    _area: (_) => or("AREA", "AREAS"),
+    _AREA: (_) => or("AREA", "AREAS"),
     _relative_file: ($) =>
       seq(
         $._select,
@@ -174,24 +178,15 @@ module.exports = grammar({
         seq($._external_or_dynamic),
         seq("DISK", "FROM", $._file_reference)
       ),
-    _indexed_file_reserve: ($) => seq("RESERVE", $.integer, $._area),
+    _indexed_file_reserve: ($) => seq("RESERVE", $.integer, $._AREA),
     _indexed_file_organization: (_) => seq(or("ORGANIZATION"), "INDEXED"),
     _indexed_file_access_mode: ($) =>
-      seq(
-        "ACCESS",
-        op("MODE"),
-        optional($._IS),
-        or("SEQUENTIAL", "RANDOM", "DYNAMIC")
-      ),
-    _record_key: ($) => seq("RECORD", op("KEY"), optional($._IS), $.data_name),
+      seq($._ACCESS_MODE_IS, or("SEQUENTIAL", "RANDOM", "DYNAMIC")),
+    _RECORD_KEY_IS: ($) => seq("RECORD", optional("KEY"), optional($._IS)),
+    _WITH_DUPLICATES: (_) => seq(op("WITH"), "DUPLICATES"),
+    _record_key: ($) => seq($._RECORD_KEY_IS, $.data_name),
     _alternate_record_key: ($) =>
-      seq(
-        "ALTERNATE",
-        op("RECORD", "KEY"),
-        optional($._IS),
-        $.data_name,
-        op(seq(op("WITH"), "DUPLICATES"))
-      ),
+      seq("ALTERNATE", $._RECORD_KEY_IS, $.data_name, op($._WITH_DUPLICATES)),
     _indexed_file: ($) =>
       seq(
         $._select,
@@ -204,7 +199,7 @@ module.exports = grammar({
         op($._file_status)
       ),
     _sort_merge_file: ($) =>
-      seq("SELECT", $.file_name, "ASSIGN", op("TO"), $._file_reference),
+      seq("SELECT", $.file_name, $._ASSIGN_TO, $._file_reference),
     input_output_control_entry: ($) =>
       or(
         $._input_output_control_rerun_entry,
@@ -231,19 +226,14 @@ module.exports = grammar({
         op("TAPE", "CONTAINS"),
         repeat(seq($.file_name, op(seq("POSITION", $.integer))))
       ),
+    _REAL_OR_UNIT: (_) => choice("REAL", "UNIT"),
+    _END_OF: (_) => seq("END", optional("OF")),
     _every: ($) =>
       seq(
         "EVERY",
         or(
-          seq(
-            or(
-              op(seq("END", op("OF"))),
-              or("REAL", "UNIT"),
-              seq($.integer, "RECORDS")
-            ),
-            "OF",
-            $.file_name
-          ),
+          seq(op($._END_OF), $._REAL_OR_UNIT, optional("OF"), $.file_name),
+          seq($.integer, "RECORDS", optional("OF"), $.file_name),
           seq($.integer, "CLOCK-UNITS"),
           /*TODO*/ "condition-name"
         )
@@ -272,23 +262,29 @@ module.exports = grammar({
       choice($._record_sequential_file_description),
     _RECORDS: ($) =>
       choice(seq("RECORD", optional($._IS)), seq("RECORDS", op("ARE"))),
-    _IS: ($) => "IS",
+    _IS: (_) => "IS",
+    _ASSIGN_TO: (_) => seq("ASSIGN", optional("TO")),
+    _IS_EXTERNAL: ($) => seq(optional($._IS), "EXTERNAL"),
+    _IS_GLOBAL: ($) => seq(optional($._IS), "GLOBAL"),
+    _LINES_AT_TOP: (_) => seq(optional("LINES"), optional("AT"), "TOP"),
+    _LINES_AT_BOTTOM: (_) => seq(optional("LINES"), optional("AT"), "BOTTOM"),
+    _CODE_SET_IS: ($) => seq("CODE-SET", optional($._IS)),
     _record_sequential_file_description: ($) =>
       seq(
         "FD",
         $.file_name,
         op(
-          seq(optional($._IS), "EXTERNAL"),
-          seq(optional($._IS), "GLOBAL"),
+          $._IS_EXTERNAL,
+          $._IS_GLOBAL,
           $._record_sequential_file_block_description,
           $._record_sequential_file_record_description,
           $._record_sequential_file_label_description,
           $._record_sequential_file_value_description,
           $._record_sequential_file_data_description,
           $._record_sequential_file_linage_description,
-          seq(op("LINES", "AT"), "TOP", $.data_name),
-          seq(op("LINES", "AT"), "BOTTOM", $.data_name),
-          seq("CODE-SET", optional($._IS), $.alphabet_name)
+          seq($._LINES_AT_TOP, $.data_name),
+          seq($._LINES_AT_BOTTOM, $.data_name),
+          seq($._CODE_SET_IS, $.alphabet_name)
         )
       ),
     _record_sequential_file_block_description: ($) =>
@@ -343,14 +339,15 @@ module.exports = grammar({
         op("LINES"),
         op(seq(op("WITH"), "FOOTING", op("AT"), $.data_name))
       ),
-    working_storage_section: ($) => "WORKING-STORAGE SECTION.",
-    linkage_section: ($) => "LINKAGE SECTION.",
-    communication_section: ($) => "COMMUNICATION SECTION.",
-    report_section: ($) => "REPORT SECTION.",
+    working_storage_section: (_) => "WORKING-STORAGE SECTION.",
+    linkage_section: (_) => "LINKAGE SECTION.",
+    communication_section: (_) => "COMMUNICATION SECTION.",
+    report_section: (_) => "REPORT SECTION.",
   },
   conflicts: ($) => [
     [$._record_sequential_file_assign, $._relative_file_assign],
     [$._sort_merge_file, $._select],
     [$._record_sequential_file_linage_description],
+    [$._record_sequential_file_assign_destination, $._relative_file_assign],
   ],
 });
